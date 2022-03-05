796cda9b654e39634e3891b5e008c6309a3e18e2
Hanif Rimamtomo <65529407+Shortfire13@users.noreply.github.com>
2022-03-05 13:28:48 +0700

    test

---
+++ app/src/main/java/com/dicoding/submission1/ListUserAdapter.kt
@@ -0,0 +1,31 @@
+package com.dicoding.submission1
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListUserAdapter(private val listUser: ArrayList<user>) : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_user, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (name, username, avatar) = listUser[position]
+        holder.imgPhoto.setImageResource(avatar)
+        holder.tvName.text = name
+        holder.tvUsername.text = username
+    }
+
+    override fun getItemCount(): Int = listUser.size
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        var tvUsername: TextView = itemView.findViewById(R.id.tv_item_username)
+    }
+}
